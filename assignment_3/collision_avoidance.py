b72915637c32e4844a389a5c255b7710d01084c5
hiteshK03 <1603coderz@gmail.com>
2022-04-09 19:35:31 +0530

    assign3

--- assignment_3/collision_avoidance.py
+++ assignment_3/collision_avoidance.py
@@ -11,258 +11,188 @@ import matplotlib.pyplot as plt
 import time
 import numpy as np
 
-class CollisionAvoidance():
-
-    def __init__(self):
-
-        self.VEL_MAX = 0.15
-        self.ANG_MAX = math.pi/18
-
-        self.VEL_DISCRETE = 15
-        self.ANG_DISCRETE = 20
-
-        self.OBS_RADIUS = 2*0.15
-
-        self.myPose = [0.0, 0.0, 0.0]
-        self.myVel = [0.0,  0.0]
-        self.myOmega = 0.0
-
-        self.obsPoseX = np.ones(3)
-        self.obsPoseY = np.ones(3)
-        self.obsPoseVx = np.ones(3)
-        self.obsPoseVy = np.ones(3)
-
-        self.coneAngles = np.zeros(3)
-
-        self.samples = []
-        self.feasiblePts = []
-
-        self.goalPoint = (5.0, 0.0)
-        self.eps = 0.5
-        self.angEps = 0.0001
-
-        self.minDev, self.minDevPt = 2*math.pi, None
-        self.maxDev, self.maxDevPt = 0, None
-
-        self.rate = rospy.Rate(30)
-
-        rospy.Subscriber('/obs_data', ObsData, self.callback_obs)
-        rospy.Subscriber('/bot_1/odom', Odometry, self.callback_odom)
-
-        self.pub_vel = rospy.Publisher('/bot_1/cmd_vel', Twist, queue_size = 10)
-
-        self.startTime = time.time()
-        self.timeKeep = []
-        self.waypoints = []
-
-
-
-    def velocity_convert(self, theta, vel_x, vel_y):
-        '''
-        Robot pose (x, y, theta)  Note - theta in (0, 2pi)
-        Velocity vector (vel_x, vel_y)
-        '''
-
-        gain_ang = 1 #modify if necessary
-        
-        ang = math.atan2(vel_y, vel_x)
-        if ang < 0:
-            ang += 2 * math.pi
-        
-        ang_err = min(max(ang - theta, - self.ANG_MAX), self.ANG_MAX)
-
-        v_lin =  min(max(math.cos(ang_err) * math.sqrt(vel_x ** 2 + vel_y ** 2), - self.VEL_MAX), self.VEL_MAX)
-        v_ang = gain_ang * ang_err
-        return v_lin, v_ang
-
-
-    def callback_odom(self, data):
-        '''
-        Get robot data
-        '''
-        self.myPose[0] = data.pose.pose.position.x
-        self.myPose[1] = data.pose.pose.position.y
-
-        self.myVel[0] = data.twist.twist.linear.x
-        self.myVel[1] = data.twist.twist.linear.y
-        self.myOmega = data.twist.twist.angular.z
-
-        quaternion = (data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w)
-        _, _, self.myPose[2] = euler_from_quaternion(quaternion)
-
-        self.timeKeep.append(time.time() - self.startTime)
-        self.waypoints.append((self.myPose[0], self.myPose[1]))
-
-
-    def clearObstacleLists(self):
-
-        self.obsPoseX = np.zeros(3)
-        self.obsPoseY = np.zeros(3)
-        self.obsPoseVx = np.zeros(3)
-        self.obsPoseVy = np.zeros(3)
-
-
-    def updateObstacleList(self, obstacle, i):
-
-        self.obsPoseX[i] = obstacle.pose_x
-        self.obsPoseY[i] = obstacle.pose_y
-        self.obsPoseVx[i] = obstacle.vel_x
-        self.obsPoseVy[i] = obstacle.vel_y
-
-
-    def callback_obs(self, data):
-        '''
-        Get obstacle data in the (pos_x, pos_y, vel_x, vel_y) for each obstacle
-        '''
-
-        # self.clearObstacleLists()
-        i = 0
-        for obstacle in data.obstacles:
-
-            self.updateObstacleList(obstacle, i)
-            i += 1
-
-
-    def dist(self, a, b):
-        return ((a[0]-b[0])**2 + (a[1]-b[1])**2)**0.5
-
-
-    def reachedGoal(self):
-        
-        return self.dist(self.goalPoint, self.myPose[:2]) < self.eps
-
-
-    def radialToCartesian(self, vel, ang, origin = (0,0)):
-
-        return (origin[0] + vel*np.cos(ang), origin[1] + vel*np.sin(ang))
-
-
-    def generateFeasibleSamples(self):
-        
-        # origin = self.myVel
-
-        theta = self.myPose[2]
-
-        self.samples = []
-
-        for vel in np.linspace(0, self.VEL_MAX, self.VEL_DISCRETE):
-
-            for ang in np.linspace(theta - self.ANG_MAX, theta + self.ANG_MAX, self.ANG_DISCRETE):
-
-                if vel != 0:
-
-                    self.samples.append(self.radialToCartesian(vel, ang))
-
-        # print(len(self.samples))
-
-
-    def generateCollisionCone(self, i):
-
-        origin = self.myPose[:2]
-        obsCentre = (self.obsPoseX[i], self.obsPoseY[i])
-        distToObs = ((origin[0]-obsCentre[0])**2 + (origin[1]-obsCentre[1])**2)**0.5
-        print(i, obsCentre, distToObs)
-        if self.OBS_RADIUS > distToObs:
-            return math.pi/2
-        # assert self.OBS_RADIUS < distToObs, "Already Collided"
-        coneAngle = np.arcsin(self.OBS_RADIUS/distToObs)
-
-        return coneAngle
-
-
-    def generateAllCollisionCones(self):
-
-        for i in range(len(self.obsPoseX)):
-            self.coneAngles[i] = self.generateCollisionCone(i)
-
-
-    def angleBetweenVectors(self, a, b):
-        if not (np.linalg.norm(a) and np.linalg.norm(b)):
-            return 0
-        return np.arccos(np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b)))
-
-    def collisionWithCone(self, pt, i):
-        
-        relV = (pt[0] - self.obsPoseVx[i], pt[1] - self.obsPoseVy[i])
-        obsVector = (self.obsPoseX[i] - self.myPose[0], self.obsPoseY[i] - self.myPose[1])
-
-        theta = self.angleBetweenVectors(relV, obsVector)
-        # print(i)
-        return abs(theta) < abs(self.coneAngles[i])
-
-
-    def inFreeSpace(self, pt):
-
-        for i in range(len(self.obsPoseX)):
-            if self.collisionWithCone(pt, i):
-                return False
-
-        return True
-
-
-    def filterPoints(self):
-
-        cones = self.generateAllCollisionCones()
-
-        self.feasiblePts = []
-
-        for pt in self.samples:
-            if self.inFreeSpace(pt):
-                self.feasiblePts.append(pt)
-        # print(len(self.feasiblePts))
-        for pt in self.feasiblePts:
-            print(pt)
-
-
-    def findOptimumPoint(self):
-
-        goalDir = (self.goalPoint[0] - self.myPose[0], self.goalPoint[1] - self.myPose[1])
-
-        self.minDev, self.minDevPt = 2*math.pi, None
-
-        for pt in self.feasiblePts:
-            currAngle = self.angleBetweenVectors(goalDir, pt)
-            if currAngle < self.minDev:
-                self.minDev = currAngle
-                self.minDevPt = pt
-
-
-    def takeStep(self, towardsGoal = False):
-
-        v_lin, v_ang = self.velocity_convert(self.myPose[2], self.minDevPt[0], self.minDevPt[1])
-
-        vel_msg = Twist()
-        vel_msg.linear.x = v_lin
-        vel_msg.angular.z = v_ang
-        self.pub_vel.publish(vel_msg)
-        # print(v_lin, v_ang)
-        self.rate.sleep()
-
-
-    def avoidObstacles(self):
-
-        while not self.reachedGoal():
-                
-
-            self.generateFeasibleSamples() # done
-
-            self.filterPoints() # done
-
-            self.findOptimumPoint() # done
-
-            self.takeStep() # done
-
-
-
-def plotData(timeKeep, waypoints):
-    wpX = [x[0] for x in waypoints]
-    wpY = [x[1] for x in waypoints]
-
-    size = min(len(wpX), len(timeKeep))
-
-    wpX, wpY, timeKeep = wpX[:size], wpY[:size], timeKeep[:size]
-
-    plt.plot(timeKeep, wpX)
+VEL_MAX = 0.15
+ANG_MAX = math.pi/18
+
+rospy.init_node('collision_avoidance', anonymous = True, disable_signals = True)
+
+params = {}
+
+params['vel_d'] = 15
+params['ang_d'] = 20
+params['obs_radi'] = 2*0.15
+
+params['origin'] = (0.0, 0.0)
+params['goal'] = (5.0, 0.0)
+params['eps'] = 0.5
+params['eps_ang'] = 0.0001
+
+params['angles_cone'] = np.zeros(3)
+params['obs_pose_x'] = params['obs_pose_y'] = np.ones(3)
+params['obs_pose_vx'] = params['obs_pose_vy'] = np.ones(3)
+
+params['my_pose'] = [0.0, 0.0, 0.0]
+params['my_vel'] = [0.0, 0.0]
+params['my_omega'] = 0.0
+
+params['samples'] = []
+params['feas_pts'] = []
+
+params['dev_min'], params['dev_min_pt'] = 2*math.pi, None
+params['dev_max'], params['dev_max_pt'] = 0, None
+
+params['rate'] = rospy.Rate(30)
+
+params['pub_vel'] = rospy.Publisher('/bot_1/cmd_vel', Twist, queue_size = 10)
+params['start'] = time.time()
+params['time_list'] = []
+params['path_list'] = []
+
+def velocity_convert(theta, vel_x, vel_y):
+    '''
+    Robot pose (x, y, theta)  Note - theta in (0, 2pi)
+    Velocity vector (vel_x, vel_y)
+    '''
+
+    gain_ang = 1 #modify if necessary
+    
+    ang = math.atan2(vel_y, vel_x)
+    if ang < 0:
+        ang += 2 * math.pi
+    
+    ang_err = min(max(ang - theta, - ANG_MAX), ANG_MAX)
+
+    v_lin =  min(max(math.cos(ang_err) * math.sqrt(vel_x ** 2 + vel_y ** 2), - VEL_MAX), VEL_MAX)
+    v_ang = gain_ang * ang_err
+    return v_lin, v_ang
+
+def callback_odom(data):
+    '''
+    Get robot data
+    '''
+
+    params['my_pose'][0] = data.pose.pose.position.x
+    params['my_pose'][1] = data.pose.pose.position.y
+
+    params['my_vel'][0] = data.twist.twist.linear.x
+    params['my_vel'][1] = data.twist.twist.linear.y
+    params['my_omega'] = data.twist.twist.angular.z
+
+    quaternion = (data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w)
+    _, _, params['my_pose'][2] = euler_from_quaternion(quaternion)
+
+    params['time_list'].append(time.time() - params['start'])
+    params['path_list'].append((params['my_pose'][0], params['my_pose'][1]))
+
+def clear_obs_params():
+	params['obs_pose_x'] = params['obs_pose_y'] = np.zeros(3)
+	params['obs_pose_vx'] = params['obs_pose_vy'] = np.zeros(3)
+	# return params
+
+def update_obs_params(obstacle, i):
+	params['obs_pose_x'][i], params['obs_pose_y'][i] = obstacle.pose_x, obstacle.pose_y
+	params['obs_pose_vx'][i], params['obs_pose_vy'][i] = obstacle.vel_x, obstacle.vel_y
+
+def callback_obs(data):
+	i = 0
+	for obstacle in data.obstacles:
+		update_obs_params(obstacle, i)
+		i+=1
+
+def euclid_dist(x1, x2):
+	return ((x1[0]-x2[0])**2 + (x1[1]-x2[1])**2)**0.5
+
+def goal_termination():
+	return euclid_dist(params['goal'], params['my_pose'][:2]) < params['eps']
+
+def radial_to_cartesian(vel, ang):
+	return (params['origin'][0]+vel*np.cos(ang), params['origin'][1]+vel*np.sin(ang))
+
+def gen_samples():
+	theta = params['my_pose'][2]
+	params['samples'] = []
+	for vel in np.linspace(0, VEL_MAX, params['vel_d']):
+		for ang in np.linspace(theta-ANG_MAX, theta+ANG_MAX, params['ang_d']):
+			if vel!=0:
+				params['samples'].append(radial_to_cartesian(vel, ang))
+
+def gen_coll_cone(i):
+	origin = params['my_pose'][:2]
+	centre_obs = (params['obs_pose_x'][i], params['obs_pose_y'][i])
+	obs_dist = ((origin[0]-centre_obs[0])**2+(origin[1]-centre_obs[1])**2)**0.5
+	print(i, centre_obs, obs_dist)
+	if params['obs_radi'] > obs_dist:
+		return math.pi/2
+	cone_angle = np.arcsin(params['obs_radi']/obs_dist)
+	return cone_angle
+
+def all_coll_cones():
+	for i in range(len(params['obs_pose_x'])):
+		params['angles_cone'][i] = gen_coll_cone(i)
+
+def ang_vectors(a, b):
+	if not (np.linalg.norm(a) and np.linalg.norm(b)):
+		return 0
+	return np.arccos(np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b)))
+
+def coll_with_cone(pt, i):
+	v_rel = (pt[0]-params['obs_pose_vx'][i], pt[1]-params['obs_pose_vy'][i])
+	obs_vector = (params['obs_pose_x'][i]-params['my_pose'][0], params['obs_pose_y'][i]-params['my_pose'][1])
+	theta = ang_vectors(v_rel, obs_vector)
+	return abs(theta) < abs(params['angles_cone'][i])
+
+def open_area(pt):
+	for i in range(len(params['obs_pose_x'])):
+		if coll_with_cone(pt,i):
+			return False
+	return True
+
+def filter_points():
+	cones = all_coll_cones()
+	params['feas_pts'] = []
+	for pt in params['samples']:
+		if open_area(pt):
+			params['feas_pts'].append(pt)
+
+	for pt in params['feas_pts']:
+		print(pt)
+
+def opti_point():
+	goal_dir = (params['goal'][0] - params['my_pose'][0], params['goal'][1]-params['my_pose'][1])
+	params['dev_min'], params['dev_min_pt'] = 2*math.pi, None
+	
+	for pt in params['feas_pts']:
+		curr_angle = ang_vectors(goal_dir, pt)
+		print(curr_angle)
+		if curr_angle<params['dev_min']:
+			params['dev_min'] = curr_angle
+			params['dev_min_pt'] = pt
+		print(params['dev_min_pt'])
+
+def proceed(towards_goal=False):
+	v_lin, v_ang = velocity_convert(params['my_pose'][2], params['dev_min_pt'][0], params['dev_min_pt'][1])
+	vel_msg = Twist()
+	vel_msg.linear.x = v_lin
+	vel_msg.angular.z = v_ang
+	params['pub_vel'].publish(vel_msg)
+	params['rate'].sleep()
+
+def avoid_obs():
+	while not goal_termination():
+		gen_samples()
+		filter_points()
+		opti_point()
+		proceed()
+
+def plotData(time_list, path_list):
+    wpX = [x[0] for x in path_list]
+    wpY = [x[1] for x in path_list]
+
+    size = min(len(wpX), len(time_list))
+
+    wpX, wpY, time_list = wpX[:size], wpY[:size], time_list[:size]
+
+    plt.plot(time_list, wpX)
     plt.xlabel("Time")
     plt.ylabel("X-coordinate")
     plt.title("X vs T")
@@ -276,14 +206,8 @@ def plotData(timeKeep, waypoints):
     plt.grid()
     plt.show()
 
+rospy.Subscriber('/obs_data', ObsData, callback_obs)
+rospy.Subscriber('/bot_1/odom', Odometry, callback_odom)
 
-rospy.init_node('collision_avoidance', anonymous = True, disable_signals = True)
-
-strategy = CollisionAvoidance()
-strategy.avoidObstacles()
-timeKeep, waypoints = strategy.timeKeep, strategy.waypoints
-plotData(timeKeep, waypoints)
-
-
-
-
+avoid_obs()
+plotData(params['time_list'], params['path_list'])
